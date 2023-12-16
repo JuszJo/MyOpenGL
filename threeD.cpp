0585354eb39f68be8d384cc09e25367e3e477da5
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-12-16 07:21:06 +0100

    add 3D to space

--- threeD.cpp
+++ threeD.cpp
@@ -59,10 +59,10 @@ void initGL(GLFWwindow* window) {
     }
 }
 
-void genVertexandBuffers(unsigned int* VAO, unsigned int* VBO, unsigned int* EBO) {
+void genVertexandBuffers(unsigned int* VAO, unsigned int* VBO) {
     glGenVertexArrays(1, VAO);
     glGenBuffers(1, VBO);
-    glGenBuffers(1, EBO);
+    // glGenBuffers(1, EBO);
 }
 
 void handleBufferObject(unsigned int VBO, float* vertices, float size) {
@@ -97,25 +97,69 @@ int main() {
 
     Shader myShader("vertexShader.glsl", "fragmentShader.glsl");
 
+    // float vertices[] = {
+    //     -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
+    //     0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+    //     -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+    //     0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
+    // };
+
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-        -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
-        0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
-    unsigned int indices[] = {
-        0, 1, 2,
-        1, 2, 3
-    };
+    // unsigned int indices[] = {
+    //     0, 1, 2,
+    //     1, 2, 3
+    // };
 
     float verticesSize = sizeof(vertices);
 
-    unsigned int VAO, VBO, EBO, TBO;
+    unsigned int VAO, VBO, TBO;
 
-    genVertexandBuffers(&VAO, &VBO, &EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
+    genVertexandBuffers(&VAO, &VBO);
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
 
     handleBufferObject(VBO, vertices, verticesSize);
 
@@ -137,22 +181,78 @@ int main() {
 
     myShader.use();
 
-    glm::mat4 transformationMatrix = glm::mat4(1.0f);
+    // glm::mat4 transformationMatrix = glm::mat4(1.0f);
 
-    transformationMatrix = glm::translate(transformationMatrix, glm::vec3(0.3f, 0.0f, 0.0f));
+    // transformationMatrix = glm::translate(transformationMatrix, glm::vec3(0.3f, 0.0f, 0.0f));
+
+    // To start drawing in 3D we'll first create a model matrix. The model matrix consists of translations, 
+    // scaling and/or rotations we'd like to apply to transform all object's vertices to the global world space. 
+    // Let's transform our plane a bit by rotating it on the x-axis so it looks like it's laying on the floor. The 
+    // model matrix then looks like this:
+    // glm::mat4 model = glm::mat4(1.0f);
+    // model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+    // Next we need to create a view matrix. We want to move slightly backwards in the scene so the object becomes visible 
+    // (when in world space we're located at the origin (0,0,0))
+    glm::mat4 view = glm::mat4(1.0f);
+    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -7.0f));
+
+    // The last thing we need to define is the projection matrix. We want to use perspective projection 
+    // for our scene so we'll declare the projection matrix like this:
+    glm::mat4 projection;
+    projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+
+    glEnable(GL_DEPTH_TEST);
+
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f), 
+        glm::vec3(-1.5f, -2.2f, -2.5f),  
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),  
+        glm::vec3(-1.7f,  3.0f, -7.5f),  
+        glm::vec3( 1.3f, -2.0f, -2.5f),  
+        glm::vec3( 1.5f,  2.0f, -2.5f), 
+        glm::vec3( 1.5f,  0.2f, -1.5f), 
+        glm::vec3(-1.3f,  1.0f, -1.5f)  
+    };
 
     while(!glfwWindowShouldClose(window)) {
         glClearColor(0.3f, 0.4f, 0.6f, 1.0f);
 
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        glUniformMatrix4fv(glGetUniformLocation(myShader.shaderProgram, "transform"), 1, GL_FALSE, glm::value_ptr(transformationMatrix));
+        // glUniformMatrix4fv(glGetUniformLocation(myShader.shaderProgram, "transform"), 1, GL_FALSE, glm::value_ptr(transformationMatrix));
+        // glm::mat4 model = glm::mat4(1.0f);
+        // model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));  
+        // glUniformMatrix4fv(glGetUniformLocation(myShader.shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(glGetUniformLocation(myShader.shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(myShader.shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         glBindTexture(GL_TEXTURE_2D, TBO);
         glBindVertexArray(VAO);
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        for(unsigned int i = 0; i < 10; ++i) {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            
+            if(i == 0) {
+                model = glm::rotate(model, (float)glfwGetTime() * -1, glm::vec3(1.0f, 1.0f, 1.0f));
+                glUniformMatrix4fv(glGetUniformLocation(myShader.shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+            }
+            else {
+                float angle = 20.0f * i;
+                model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+
+                glUniformMatrix4fv(glGetUniformLocation(myShader.shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+            }
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+        // glDrawArrays(GL_TRIANGLES, 0, 36);
+        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         processInput(window);
 
--- vertexShader.glsl
+++ vertexShader.glsl
@@ -2,11 +2,14 @@
 layout (location = 0) in vec3 pos;
 layout (location = 1) in vec2 textureCoord;
 
-uniform mat4 transform;
+// uniform mat4 transform;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 out vec2 myTextureCoord;
 
 void main() {
-    gl_Position = transform * vec4(pos, 1.0);
+    gl_Position = projection * view * model * vec4(pos, 1.0);
     myTextureCoord = textureCoord;
 }
